1c6883cff937a82e64a3a544f1399d6bd8b0ee0d
danericp <daneric.pelayo@gmail.com>
2023-12-12 17:18:00 -0500

    Ongoing Changes

--- JDBCOracleCRUD/src/crud/ManageImage.java
+++ JDBCOracleCRUD/src/crud/ManageImage.java
@@ -26,7 +26,14 @@ public class ManageImage {
 			ResultSet rs = st.executeQuery("");
 			while(rs.next()) {
 				
-				ImageObject image = new ImageObject(rs.getInt("id"), rs.getString("img_name"), rs.getString("img_description"), rs.getString("img_type"), rs.getDate("date_created"));
+				ImageObject image = new ImageObject(
+					rs.getInt("id"),
+					rs.getBlob(0)
+					rs.getString("img_name"),
+					rs.getString("img_description"),
+					rs.getString("img_type"),
+					rs.getDate("date_created")
+				);
 				imageList.add(image);
 				
 			}
@@ -52,7 +59,14 @@ public class ManageImage {
 			ResultSet rs = ps.executeQuery();
 			while(rs.next()) {
 				
-				image = new ImageObject(rs.getInt("id"), rs.getString("img_name"), rs.getString("img_description"), rs.getString("img_type"), rs.getDate("date_created"));
+				image = new ImageObject(
+					rs.getInt("id"),
+					rs.getString("img_name"),
+					rs.getString("img_description"),
+					rs.getString("img_type"),
+					rs.getDate("date_created")
+				);
+
 			}
 			
 		}
@@ -77,7 +91,6 @@ public class ManageImage {
 			ps.setString(2, null);
 			ps.setString(3, null);
 			
-			
 		}
 		catch (Exception e) {
 			
@@ -85,34 +98,6 @@ public class ManageImage {
 			
 		}
 		return status;
-		
-		
-		
-		
-		
-		
-		
-		
-		try {
-			System.out.println("ImageStorageTest INSERT");
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection obj_conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "player");
-			Integer int_n = 0;
-			FileInputStream obj_fis = new FileInputStream("C:\\Users\\daner\\Downloads\\huh.jpg");
-			PreparedStatement obj_ps = obj_conn.prepareStatement("INSERT INTO imagetest VALUES (?, ?)");
-			obj_ps.setString(1, "Huh Cat");
-			obj_ps.setBinaryStream(2, obj_fis);
-			int_n += obj_ps.executeUpdate();
-			System.out.println("Number of rows inserted: " + int_n);
-			obj_conn.close();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally {
-			System.out.println("ImageStorageTest INSERT... Complete");
-		}
-		
 		
 	}
 
---
+++ JDBCOracleCRUD/src/obj/Initialization.java
@@ -0,0 +1,20 @@
+package obj;
+
+public class Initialization {
+
+	public final static String _DB_TABLE = "tb_crud_image";
+	public final static String _DB_TABLE_COL1_N_ID = "id";
+	public final static String _DB_TABLE_COL2_N_CONTENT = "img_content";
+	public final static String _DB_TABLE_COL3_N_NAME = "img_name";
+	public final static String _DB_TABLE_COL4_N_DESC = "img_desc";
+	public final static String _DB_TABLE_COL5_N_TYPE = "img_type";
+	public final static String _DB_TABLE_COL6_N_CREATED = "date_created";
+	
+	// Integer imgId;
+	// Blob imgContent;
+	// String imgName;
+	// String imgDescription;
+	// String imgType;
+	// Date dateCreated;
+
+}
